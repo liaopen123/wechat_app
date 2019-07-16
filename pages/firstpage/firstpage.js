5d0a59bd6344805c2515143fe36c34b5e1067595
廖鹏辉 <467969@qq.com>
2019-07-16 22:07:20 +0800

    initial commit wxapp
    add template  basic component use ,data bind and render tag

---
+++ pages/firstpage/firstpage.js
@@ -0,0 +1,77 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    love:"爱就得克制，不爱就得放肆",
+    hate:"我恨我爱你",
+    isShow:true,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+  donext: function () {
+    console.log("爱你一万年，小雪糕 ");
+    this.setData(
+      {
+        love:"雪糕最美丽~~",
+        isShow:!this.data.isShow,
+      }
+    );
+  },
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+
+
+})
