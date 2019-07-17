5db8540636c06f21eb66ede5bb7bd5923060c8ec
廖鹏辉 <467969@qq.com>
2019-07-17 17:11:53 +0800

    添加页面传值

---
+++ pages/sendparamstodetails/index.js
@@ -0,0 +1,80 @@
+// pages/sendparamstodetails/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  clickArtical1:function(){
+    wx.navigateTo({
+      url: '../getparamsindetails/index?id=最爱小雪糕',
+    })
+  },
+  clickArtical2: function () {
+
+  },
+  clickArtical3: function () {
+
+  },
+  clickArtical4: function () {
+
+  },
+})
